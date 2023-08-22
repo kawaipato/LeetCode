4ba91ceff14aecf8ebe322552cfdbce20a186df5
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2023-08-22 19:11:39 +0900

    Time: 1291 ms (40.57%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,4 @@
 /* Write your PL/SQL query statement below */
-select a.id from weather a
-where a.temperature > (select b.temperature from weather b where b.recorddate = a.recorddate -1 )
+select id from
+(select id, recordDate, temperature, lag(temperature) over (order by recorddate) as temperature_y, lag(recorddate) over (order by recorddate) as recorddate_y from weather order by recorddate)
+where temperature > temperature_y and recorddate = recorddate_y + 1;
