202a74c1883be7f9fe08e38899dfed9ad1baf26e
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-03-28 01:33:52 +0900

    Time: 673 ms (52.86%), Space: 0B (100.00%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,12 +1,14 @@
-with stadium_with_rnk as(
-    (select id, visit_date, people, rnk, (id - rnk) as island
-    from (select id, visit_date, people, rank() over(order by id) as rnk
+with base as (
+    select *,
+        lead(id, 1) over(order by id) as next_id,
+        lead(id, 2) over(order by id) as sec_next_id,
+        lag(id, 1) over(order by id) as last_id,
+        lag(id, 2) over(order by id) as sec_last_id
     from stadium
-    where people >= 100) as T))
+    where people >= 100)
     
-select id, visit_date, people from stadium_with_rnk
-where island in (select island
-                from stadium_with_rnk
-                group by island
-                having count(*) >= 3)
+select id, visit_date, people from base
+where (next_id - id = 1 and sec_next_id - next_id = 1)
+    or (next_id - id = 1 and id - last_id = 1)
+    or (id - last_id = 1 and last_id - sec_last_id = 1)
 order by id
