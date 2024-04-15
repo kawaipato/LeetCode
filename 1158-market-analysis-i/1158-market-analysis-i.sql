2f0e4017a19ab6f6e1797a344c3cf7d9843a6181
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-04-15 21:39:55 +0900

    Time: 2184 ms (69.55%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,4 +1,4 @@
-select u.user_id as buyer_id, join_date, count(o.order_id) as orders_in_2019
-from Orders O right join Users U on O.buyer_id = U.user_id and year(order_date) = 2019
+select user_id as buyer_id, join_date, sum(case when year(order_date) = 2019 then 1 else 0 end) as orders_in_2019
+from Orders O right join Users U on O.buyer_id = U.user_id
 group by user_id
 order by user_id
