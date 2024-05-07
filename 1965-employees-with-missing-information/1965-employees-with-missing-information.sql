ff42f74462cc1ed61832f8be5f1b62f5ff4df6f8
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-05-08 00:22:32 +0900

    Time: 1259 ms (57.66%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,7 +1,10 @@
-select T.employee_id
-from
-(select * from employees left join salaries using(employee_id)
-union
-select * from employees right join salaries using(employee_id)) as T
-where T.salary is null or T.name is null
-order by employee_id
+SELECT e.employee_id 
+  FROM Employees e 
+  LEFT JOIN Salaries s ON e.employee_id = s.employee_id
+  WHERE s.salary IS NULL
+  UNION
+ SELECT s1.employee_id
+   FROM Salaries s1
+   LEFT JOIN Employees e1 ON e1.employee_id = s1.employee_id
+  WHERE e1.name IS NULL
+ 
